bf9115966c7342659f6c9a863415ca60b2722de0
Todun01 <obafemitodunni@gmail.com>
2025-07-05 10:50:58 +0100

    update functionality implemented & working

--- rust_app/src/lib.rs
+++ rust_app/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, f32::consts::E, io::{self, Write}, process};
+use std::{error::Error, f32::consts::E, io::{self, Write}, process::{self, exit}};
 use bcrypt::{hash, verify, DEFAULT_COST};
 use dotenvy::dotenv;
 use sqlx::{pool, postgres::PgPoolOptions, Pool, Postgres};
@@ -48,20 +48,27 @@ impl Args  {
         if _args.len() > 2{
             return Err(format!("You entered too many arguments"))
         }
-        let init_command = _args[0].clone();
-        let query = _args[1].clone();
+        let init_command = _args[0].clone().to_lowercase();
+        let query = _args[1].clone().to_lowercase();
         if init_command != "tracer"{
             return Err(format!("{} is not a recognized command", init_command))
         }
         let allowed_queries = vec!["log".to_string(), 
         "view".to_string(), "update".to_string()];
         if !allowed_queries.contains(&query){
+            println!("Allowed commands: {}", allowed_queries.join(","));
             return Err(format!("{} is not a recognized command", query))
         }
         Ok(Args{init_command, query})
     }
 }
 
+pub fn get_response()-> Result<String, Box<dyn Error>>{
+    let mut response = String::new();
+    io::stdin().read_line(&mut response).expect("Failed to read response");
+
+    Ok(response)
+}
 pub async fn start() -> Pin<Box<dyn Future<Output = Result<(), Box<dyn Error>>>>>{
     dotenv().ok();
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
@@ -314,8 +321,139 @@ pub async fn view(user_id: i32, POOL: Pool<Postgres>)->Result<(), Box<dyn Error>
     }
     Ok(())
 }
-pub async fn update(){
+pub async fn update(user_id: i32, pooL:Pool<Postgres>)-> Pin<Box<dyn Future<Output = Result<(), Box<dyn Error>>>>>{
+    println!("Update the status of a bug.");
+    io::stdout().flush().unwrap();
+    Box::pin(async move{
+    view(user_id, pooL.clone()).await?;
 
+    println!("Enter the name of the bug you want to update:");
+    io::stdout().flush().unwrap();
+
+    let bug_name = get_response().unwrap().trim().to_string();
+
+    let id = sqlx::query!(
+        "SELECT id FROM bugs WHERE name = $1 AND user_id = $2",
+        bug_name,
+        user_id
+    )
+    .fetch_optional(&pooL)
+    .await?;
+    match id{
+        Some(record) => {
+            let bug_id = record.id;
+            println!("Bug found!");
+            println!("What would you like to update?(Status/Description)");
+            io::stdout().flush().unwrap();
+
+            let response = get_response().unwrap().trim().to_string();
+
+            if response.to_lowercase() == "status"{
+                println!("NOTE: Closed bugs are automatically deleted after 24 hours.");
+                println!("PLease update the status of '{}'(Open/Closed):", bug_name);
+                io::stdout().flush().unwrap();
+
+                let status = get_response().unwrap().trim().to_string();
+
+                let bug_status = sqlx::query!(
+                    "SELECT status FROM bugs WHERE id = $1",
+                    bug_id
+                ).fetch_one(&pooL)
+                .await?;
+                let allowed_commands: Vec<String> = vec!["open".to_string(), "closed".to_string()];
+                if allowed_commands.contains(&status){
+                    if 
+                        bug_status.status.to_lowercase() == 
+                        status.trim().to_lowercase(){
+                            println!("Bug is already {}", status);
+                            println!("Do you still wish to update?(Y/n");
+                            io::stdout().flush().unwrap();
+
+                            let response = get_response().unwrap().trim().to_string();
+                            if response.eq_ignore_ascii_case("Y"){
+                                update(user_id, pooL.clone()).await.await?;
+                                
+                            } else if response.eq_ignore_ascii_case("n"){
+                                process::exit(1);
+                            } else{
+                                eprintln!("{} is not a recognized response", response);
+                                process::exit(1);
+                            }
+                        } else{
+                            let update = sqlx::query!(
+                                "UPDATE bugs SET status = $1, updated_at = CURRENT_TIMESTAMP WHERE id = $2 AND user_id = $3",
+                                status.trim(),
+                                bug_id,
+                                user_id
+                            )
+                            .execute(&pooL)
+                            .await?;
+                            if update.rows_affected() == 0{
+                                eprintln!("Failed to update status.");
+                                process::exit(1);
+                            } else{
+                                println!("Status updated successfully!");
+                                view(user_id, pooL.clone()).await?;
+                                process::exit(1);
+                            }
+                        }
+                } else{
+                    println!("{} is not a recognized command.", status);
+                    process::exit(1);
+                }
+                
+            } else if response.to_lowercase() == "description"{
+                println!("Please update the description of '{}':", bug_name);
+                io::stdout().flush().unwrap();
+
+                let mut description = String::new();
+                loop {
+                    let mut line = String::new();
+                    io::stdin().read_line(&mut line)?;
+                    if line.trim() == "END" {
+                        break;
+                    }
+                    description.push_str(&line);
+                }
+                let update = sqlx::query!(
+                    "UPDATE bugs SET description = $1, updated_at = CURRENT_TIMESTAMP WHERE id = $2 AND user_id = $3",
+                    description.trim(),
+                    bug_id,
+                    user_id
+                )
+                .execute(&pooL)
+                .await?;
+                if update.rows_affected() == 0{
+                    eprintln!("Failed to update description.");
+                    process::exit(1);
+                } else{
+                    println!("Description updated successfully!");
+                    view(user_id, pooL.clone()).await?;
+                    process::exit(1);
+                }
+            } else{
+                eprintln!("{} is not a recognized command", response);
+                process::exit(1);
+            }
+            
+        },
+        None => {
+            eprintln!("'{}' is not logged under this user. Do you wish to log it?(Y/n)", bug_name);
+            io::stdout().flush().unwrap();
+
+            let response = get_response().unwrap().trim().to_string();
+            if response.eq_ignore_ascii_case("Y"){
+                log(user_id, pooL).await.await?
+            } else if response.eq_ignore_ascii_case("n") {
+                process::exit(1);
+            } else{
+                eprintln!("{} is not a recognized response", response);
+                process::exit(1);
+            }
+        }
+    }
+    Ok(())
+    })
 }
 pub async fn run(items:&[String]) -> Result<(), Box<dyn Error>>{
     let _args = Args::parse_args(items).unwrap_or_else(|err| {
@@ -338,11 +476,13 @@ pub async fn run_in_session<'a>(items:&[String], user:AuthUser<'a>, pool:Pool<Po
     if _args.query == "log"{
         log(*user.id, pool.clone()).await.await.expect("Failed to log");
     }
-    if _args.query == "view"{
+    else if _args.query == "view"{
         if let Err(e) = view(*user.id, pool.clone()).await{
             eprintln!("Application error: {}", e);
             process::exit(1)
         } 
+    } else if _args.query == "update"{
+        update(*user.id, pool.clone()).await.await.expect("Failed to update.")
     }
     Ok(())
 }
@@ -363,4 +503,11 @@ fn login_test(){
 fn hasing(){
     let hashed = hash("1234", DEFAULT_COST).unwrap();
     println!("Hashed: {}", hashed);
+}
+
+#[test]
+
+fn response_test(){
+    let response = get_response().unwrap();
+    println!("{}", response)
 }
---
+++ rust_app/target/debug/deps/rust_app-c0b71d84a8dd6da0.d
@@ -0,0 +1,5 @@
+/home/oluwatodunni/Documents/CLI-Bug-Tracker/rust_app/target/debug/deps/rust_app-c0b71d84a8dd6da0: src/lib.rs
+
+/home/oluwatodunni/Documents/CLI-Bug-Tracker/rust_app/target/debug/deps/rust_app-c0b71d84a8dd6da0.d: src/lib.rs
+
+src/lib.rs:
